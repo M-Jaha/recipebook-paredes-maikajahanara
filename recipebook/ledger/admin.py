7f132decfe18f3faca2ee3c4214de06a94bb1092
Phisograph <jahanaraparedes@gmail.com>
2026-02-19 23:19:13 +0800

    Updated templates and admin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Recipe, RecipeIngredient, Ingredient
 
-# Register your models here.
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+
+admin.site.register(Ingredient)
