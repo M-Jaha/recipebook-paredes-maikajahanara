9fb7df2d8b9995b01853ada615884da8b456d427
Phisograph <jahanaraparedes@gmail.com>
2026-03-02 23:01:55 +0800

    Fixed files as per comments

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('recipes/', views.recipe_list, name='recipe-list'),
-    path('recipes/<int:pk>/', views.recipe_detail, name='recipe-detail'),
+    path('recipes/list/', views.recipe_list, name='recipe_list'),
+    path('recipes/<int:pk>/', views.recipe_detail, name='recipe_detail'),
 ]
